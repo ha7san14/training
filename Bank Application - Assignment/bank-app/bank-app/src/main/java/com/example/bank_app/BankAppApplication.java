7092d4dafac72559e3f0841cc4dbe5b10c298743
Muhammad Hassan Ikram <bcsf20m525@pucit.edu.pk>
2024-07-31 16:02:16 +0500

    env check

--- Bank Application - Assignment/bank-app/bank-app/src/main/java/com/example/bank_app/BankAppApplication.java
+++ Bank Application - Assignment/bank-app/bank-app/src/main/java/com/example/bank_app/BankAppApplication.java
@@ -1,12 +1,15 @@
 package com.example.bank_app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankAppApplication {
-public static void main(String[] args) {
-SpringApplication.run(BankAppApplication.class, args);
-}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+        SpringApplication.run(BankAppApplication.class, args);
+    }
 }
 
